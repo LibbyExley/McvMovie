6212d86c15e16064acfa7f87afd69c1ffa6873c5
Libby Exley <libby@libbys-air.home>
2019-09-09 19:28:14 +0100

    change provider to sqlite

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,7 @@ namespace McvMovie
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<McvMovieContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("McvMovieContext")));
+                    options.UseSqlite("Data Source=movies.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
